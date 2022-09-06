f6b3dc2b24a7c1a2e05f7828cac9b4d244ed4f91
shubhampitweb <shubhampatil.itweb@gmail.com>
2022-09-06 18:14:33 +0530

    covid tab & testimonials star remove

--- group-tour-detail.php
+++ group-tour-detail.php
@@ -250,12 +250,6 @@ style="background-image: url(../images/title-background.jpg); -->
 
                     </li>
 
-                    <li class="nav-item">
-
-                        <a class="nav-link" id="pills-covid-tab" data-toggle="pill" href="#pills-covid" role="tab" aria-controls="pills-covid" aria-selected="false">Covid 19</a>
-
-                    </li>
-
                 </ul>
 
                 <div class="tab-content ts-tab-content" id="pills-tabContent">
@@ -667,17 +661,7 @@ style="background-image: url(../images/title-background.jpg); -->
                         </div>
 
                     </div>
-
-                    <div class="tab-pane fade" id="pills-covid" role="tabpanel" aria-labelledby="pills-covid-tab">
-
-                        <div class="ts-tab-content__inner">
-
-                            <p>Static Content..</p>
-
-                        </div>
-
-                    </div>
-
+                    
                 </div>
 
             </div>
