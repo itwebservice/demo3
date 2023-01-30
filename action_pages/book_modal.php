0e1cc6cf5fe9402770bdbb086fe58b1faaa51c70
SonamItweb <sonam.itweb@gmail.com>
2023-01-30 13:04:31 +0530

    b2c taxation done

--- action_pages/book_modal.php
+++ action_pages/book_modal.php
@@ -68,7 +68,7 @@ $costing_desc = strip_tags($costing_desc);
                 <input type="hidden" id="coupon_codes" value='<?= ($coupon_codes) ?>' />
                 <div class="form-row">
                     <div class="form-group col-md-4" style="border-right:2px solid gray;">
-                        <label> Package Name: <?= $package_name.'('.$package_typef.')' ?> </label>
+                        <label> Tour Name: <?= $package_name.'('.$package_typef.')' ?> </label>
                     </div>
                     <div class="form-group col-md-4" style="border-right:2px solid gray;">
                         <label> Travel Date: <?= $travel_from.' To '.$travel_to ?> </label>
@@ -239,7 +239,7 @@ $costing_desc = strip_tags($costing_desc);
                         <label>*Select State</label>
                         <?php
                         $service = ($type == '1') ? 'Package Tour' : 'Group Tour'; ?>
-                        <select name="state" id="state" title="Select State" style="width : 100%" onchange="get_tax(this.id,'total_cost','<?= $service ?>');">
+                        <select name="state" id="state" title="Select State/Country" style="width : 100%" onchange="get_tax(this.id,'total_cost','<?= $service ?>');">
                         <?php get_states_dropdown() ?>
                         </select>
                     </div>
@@ -462,22 +462,29 @@ function get_service_tax(result,cust_state) {
                 });
         });
     ////////////////////////////////////////
-    var applied_taxes = '';
-    var ledger_posting = '';
-    applied_rules && applied_rules.map((rule) => {
-        
-        var tax_data = taxes_result.find((entry_id_tax) => entry_id_tax['entry_id'] === rule['entry_id']);
-
-        var { rate_in, rate } = tax_data;
-        var { ledger_id, name } = rule;
-        if (applied_taxes != '') {
-            applied_taxes = applied_taxes + '+' + name + ':' + rate + ':' + rate_in;
-            ledger_posting = ledger_posting + '+' + ledger_id;
-        } else {
-            applied_taxes += name + ':' + rate + ':' + rate_in;
-            ledger_posting += ledger_id;
-        }
-    });
+		var applied_taxes = '';
+		var ledger_posting = '';
+		applied_rules && applied_rules.map((rule) => {
+			var tax_data = taxes_result.find((entry_id_tax) => entry_id_tax['entry_id'] === rule['entry_id']);
+			var {  ledger1,ledger2, name1,name2,amount1,amount2 } = tax_data;
+			var { name } = rule;
+			if (applied_taxes == '') {
+				applied_taxes = name1 + ':' + amount1 + ':' + 'Percentage';
+				ledger_posting = ledger1;
+				if (name2 != '') {
+					applied_taxes += '+' + name2 + ':' + amount2 + ':' + 'Percentage';
+					ledger_posting += '+' + ledger2;
+				}
+			}
+			else {
+				applied_taxes += name1 + ':' + amount1 + ':' + 'Percentage';
+				ledger_posting = ledger_posting + '+' + ledger1;
+				if (name2 != '') {
+					applied_taxes += '+' + name2 + ':' + amount2 + ':' + 'Percentage';
+					ledger_posting += '+' + ledger2;
+				}
+			}
+		});
     return applied_taxes + ',' + ledger_posting;
 }
 function costing_calc(){
